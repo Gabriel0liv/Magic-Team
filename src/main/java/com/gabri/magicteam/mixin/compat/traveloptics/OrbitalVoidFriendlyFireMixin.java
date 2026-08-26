6c3fdfe5178ef9fe64aef28cc154ab69c80591d7
Gabriel Oliveira <148448623+Gabriel0liv@users.noreply.github.com>
2026-08-26 20:12:45 +0100

    fix: cover Travel Optics orbital void targeting

---
+++ src/main/java/com/gabri/magicteam/mixin/compat/traveloptics/OrbitalVoidFriendlyFireMixin.java
@@ -0,0 +1,21 @@
+package com.gabri.magicteam.mixin.compat.traveloptics;
+
+import com.gabri.magicteam.util.TeamUtils;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Pseudo
+@Mixin(targets = "com.gametechbc.traveloptics.spells.ender.OrbitalVoidSpell", remap = false)
+public abstract class OrbitalVoidFriendlyFireMixin {
+    @Redirect(
+            method = "isValidTarget(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/LivingEntity;)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;m_7307_(Lnet/minecraft/world/entity/Entity;)Z", remap = false),
+            remap = false
+    )
+    private boolean magicTeam$useFriendlyFirePolicy(Entity caster, Entity target) {
+        return TeamUtils.shouldBlockFriendlyFire(caster, target);
+    }
+}
