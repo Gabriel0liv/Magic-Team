a8ec1e11dd38920ecd305272bea4447620822276
Gabriel Oliveira <148448623+Gabriel0liv@users.noreply.github.com>
2026-08-27 01:03:57 +0100

    test: require Cataclysm axe blade friendly-fire adapter

--- src/test/java/com/gabri/magicteam/mixin/MixinWiringContractTest.java
+++ src/test/java/com/gabri/magicteam/mixin/MixinWiringContractTest.java
@@ -29,7 +29,8 @@ public final class MixinWiringContractTest {
             "compat.traveloptics.HarbingersWrathFriendlyFireMixin",
             "compat.traveloptics.CataclysmFlameJetFriendlyFireMixin",
             "compat.traveloptics.CataclysmAmethystClusterFriendlyFireMixin",
-            "compat.traveloptics.CataclysmVoidRuneFriendlyFireMixin"
+            "compat.traveloptics.CataclysmVoidRuneFriendlyFireMixin",
+            "compat.traveloptics.CataclysmAxeBladeFriendlyFireMixin"
     );
 
     private MixinWiringContractTest() {
